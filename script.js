69b6933a373ca83897473955f892884454b89238
MJ93277 <mariajames93277@gmail.com>
2026-07-18 14:15:21 +0530

    arranged code in professional way

--- script.js
+++ script.js
@@ -1,72 +1,75 @@
+
+
+
+
 const pushEl = document.querySelector('.push');
 const inputEl = document.querySelector('input');
 const containerEl = document.querySelector('.container');
 // let taskTitle;
 
 
-pushEl.addEventListener('click', () => {
+pushEl.addEventListener('click', addTask);
+
+function addTask(){
    
     if(inputEl.value === ''){
         alert("Empty task cannot be add");
     }else{
-        
-            const taskList = document.createElement('div');
-            taskList.classList.add('task-list');
-            // taskList.style.boxShadow = "0px -3px 1px rgba(255, 25, 0, 0.9) inset, 0px -3px 15px rgba(8, 8, 8, 0.3)";
-            taskList.style.boxShadow = "0px -3px 1px rgba(255, 25, 0, 0.9) inset, 0px 3px 1px rgb(255, 25, 0) inset";
-            containerEl.append(taskList)
-
-
-            
-
-            const taskTitle = document.createElement('span')
-            taskTitle.classList.add('task-title');
-            taskTitle.textContent = inputEl.value;
-            taskList.append(taskTitle);
-
-
-            const deleteBtn = document.createElement('button');
-            deleteBtn.classList.add('deletebtn');
-            deleteBtn.innerHTML = `<i class="ri-delete-bin-line" id="delete"></i> `;
-            taskList.append(deleteBtn);
-
+        pushTask();
         inputEl.value = '';
-
-      
-
-
         }
     
-});
+}
 
-
-    const taskTitle = document.querySelectorAll('.container div span');
-    const taskList = document.querySelectorAll('.container .task-list')
+function pushTask(){
+    const taskList = document.createElement('div');
     
-        let divColor = "red";
+    taskList.classList.add('task-list');
+    containerEl.append(taskList);
 
-containerEl.addEventListener('click', (event) => {
+    const taskTitle = document.createElement('span')
+    taskTitle.classList.add('task-title');
+    taskTitle.textContent = inputEl.value;
+    taskList.append(taskTitle);
+
+    const deleteBtn = document.createElement('button');
+    deleteBtn.classList.add('deletebtn');
+    deleteBtn.innerHTML = `<i class="ri-delete-bin-line" id="delete"></i> `;
+    taskList.append(deleteBtn);
+}
+
+
+
+containerEl.addEventListener('click', handleTaskClick);
+
+function handleTaskClick () {
 
     if (event.target.classList.contains('task-title')) {
-
         const currentTask = event.target.closest('.task-list');
-        
-        // console.log(currentTask.style.boxShadow, "\n", divColor)
-        console.log(divColor)
-        if(divColor!== "green"){
-            currentTask.style.boxShadow =  "0px -3px 1px rgb(48, 221, 0) inset, 0px 3px 1px rgb(48, 221, 0) inset";
-            divColor = "green";
-            console.log("true")
+        // console.log(currentTask.classList.toggle("completed"));
+        if(currentTask.classList.toggle("completed")){
+            redToGreen(currentTask);
         }else{
-            currentTask.style.boxShadow = "0px -3px 1px rgba(255, 25, 0, 0.9) inset, 0px 3px 1px rgb(255, 25, 0) inset";
-            divColor = "red"
-            console.log("false")
-        }
+            greenToRed(currentTask);
+           }   
     }
 
     const deleteBtn = event.target.closest(".deletebtn");
     if(deleteBtn){
-       event.target.closest('div').style.display = "none";
+       deleteTask(deleteBtn);
     }
-});
+};
 
+function redToGreen(currentTask){
+    currentTask.style.boxShadow =  "0px 6px 1px rgb(48, 221, 0) , 0px -3px 15px rgba(8, 8, 8, .3) ";
+
+}
+
+function greenToRed(currentTask){
+    currentTask.style.boxShadow = " 0px 6px 1px rgba(255, 25, 0, 0.9) ,  0px -3px 15px rgba(8, 8, 8, 0.3)";
+
+}
+
+function deleteTask(deleteBtn){
+    deleteBtn.closest('.task-list').remove();
+}
